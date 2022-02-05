2595a81fc92559ad5c1d3e341e77f4e31e26ac98
nayru <nanualexandraramirez@gmail.com>
2022-02-05 09:12:37 -0500

    resolve

--- src/components/pages/colection.js
+++ src/components/pages/colection.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ColumnZero from '../components/ColumnZero';
-import ColumnZeroTwo from '../components/ColumnZeroTwo';
 import Footer from '../components/footer';
 import { createGlobalStyle } from 'styled-components';
 import Account from './account';
@@ -22,24 +21,8 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
-const Colection= function() {
-const [openMenu, setOpenMenu] = React.useState(true);
-const [openMenu1, setOpenMenu1] = React.useState(false);
-const handleBtnClick = (): void => {
-  setOpenMenu(!openMenu);
-  setOpenMenu1(false);
-  document.getElementById("Mainbtn").classList.add("active");
-  document.getElementById("Mainbtn1").classList.remove("active");
-};
-const handleBtnClick1 = (): void => {
-  setOpenMenu1(!openMenu1);
-  setOpenMenu(false);
-  document.getElementById("Mainbtn1").classList.add("active");
-  document.getElementById("Mainbtn").classList.remove("active");
-};
-
-
-
+const Colection= function(props) {
+  console.log(props.location.aboutProps);
 return (
 <div>
 <GlobalStyles/>
@@ -55,22 +38,14 @@ return (
           <div className='col-lg-12'>
               <div className="items_filter">
                 <ul className="de_nav">
-                    <li id='Mainbtn' className="active"><span onClick={handleBtnClick}>On sale</span></li>
-                    <li id='Mainbtn1' className=""><span onClick={handleBtnClick1}>Owned</span></li>
+                    <li id='Mainbtn' className="active"><span>Your tokens</span></li>
                 </ul>
             </div>
           </div>
         </div>
-      {openMenu && (  
         <div id='zero1' className='onStep fadeIn'>
          <ColumnZero/>
         </div>
-      )}
-      {openMenu1 && ( 
-        <div id='zero2' className='onStep fadeIn'>
-         <ColumnZeroTwo/>
-        </div>
-      )}
       </section>
 
 
